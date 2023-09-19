ce9f2370929b023102ef65723d8531b05d562c3e
Tristan Laulanie <laulanietristan@gmail.com>
2023-09-19 15:45:19 +0200

    Couleurs JS : Ready

---
+++ app.js
@@ -0,0 +1,125 @@
+// Initialisation : Afficher 'Hello World!' dans la console
+console.log('Hello World!');
+
+// Sélectionner le body pour la mise à jour du background
+let body = document.querySelector('body');
+
+// Sélectionner le premier input couleur
+let firstColorInput = document.querySelector('.first-color-input');
+// Sélectionner le second input couleur
+let secondColorInput = document.querySelector('.second-color-input');
+
+// Sélectionner l'input range pour l'orientation du gradient
+let rangeInput = document.querySelector('.input-range');
+
+// Sélectionner la div d'affichage de la première couleur
+let firstColorValueDisplay = document.querySelector('#first-color-label');
+// Sélectionner la div d'affichage de la seconde couleur
+let secondColorValueDisplay = document.querySelector('#second-color-label');
+
+// Sélectionner la div d'affichage de l'orientation
+let orientationValue = document.querySelector('.orientation-value');
+
+// Sélectionner le bouton de copie
+let copyButton = document.querySelector('.copy-button');
+
+// Sélectionner le bouton pour générer des couleurs aléatoires
+let randomButton = document.querySelector('.random-button');
+
+// Initialiser les variables pour les valeurs des couleurs et de l'orientation
+let firstColorValue, secondColorValue;
+let rangeValue;
+
+// Fonction pour mettre à jour le gradient du body
+function updateGradient() {
+    // Vérifier si toutes les valeurs sont présentes
+    if (firstColorValue && secondColorValue && rangeValue) {
+        // Construire la string du gradient
+        let gradient = `linear-gradient(${rangeValue}deg, ${firstColorValue}, ${secondColorValue})`;
+        // Appliquer le gradient au body
+        body.style.background = gradient;
+        // Log pour le débogage
+        console.log("Gradient appliqué:", gradient);
+    }
+}
+
+// Écouteur d'événement pour le premier input couleur
+firstColorInput.addEventListener('change', function(event) {
+    // Stocker la nouvelle valeur de couleur
+    firstColorValue = event.target.value;
+    // Afficher la couleur dans la div correspondante
+    firstColorValueDisplay.textContent = firstColorValue.toUpperCase();
+    // Appliquer le nouveau gradient
+    updateGradient();
+});
+
+// Écouteur d'événement pour le second input couleur
+secondColorInput.addEventListener('change', function(event) {
+    // Stocker la nouvelle valeur de couleur
+    secondColorValue = event.target.value;
+    // Afficher la couleur dans la div correspondante
+    secondColorValueDisplay.textContent = secondColorValue.toUpperCase();
+    // Appliquer le nouveau gradient
+    updateGradient();
+});
+
+// Écouteur d'événement pour l'input range
+rangeInput.addEventListener('change', function(event) {
+    // Stocker la nouvelle valeur de l'orientation
+    rangeValue = event.target.value;
+    // Afficher la nouvelle orientation
+    orientationValue.textContent = 'Orientation: ' + rangeValue + '°';
+    // Appliquer le nouveau gradient
+    updateGradient();
+});
+
+// Fonction pour copier du texte dans le presse-papiers
+function copyToClipboard(text) {
+    // Utiliser l'API Clipboard pour la copie
+    navigator.clipboard.writeText(text)
+        .then(() => {
+            // Log en cas de réussite
+            console.log('Texte copié:', text);
+        })
+        .catch(err => {
+            // Log en cas d'erreur
+            console.error('Erreur de copie:', err);
+        });
+}
+
+// Écouteur d'événement pour le bouton de copie
+copyButton.addEventListener('click', function(event) {
+    // Vérifier si le gradient est complet
+    if (firstColorValue && secondColorValue && rangeValue) {
+        // Construire la string du gradient
+        let gradient = `linear-gradient(${rangeValue}deg, ${firstColorValue}, ${secondColorValue})`;
+        // Copier le gradient
+        copyToClipboard(gradient);
+    } else {
+        // Log d'avertissement en cas d'infos manquantes
+        console.warn("Infos de gradient incomplètes.");
+    }
+});
+
+// Fonction pour générer une couleur hexadécimale aléatoire
+function randomHexColor() {
+    // Fonction pour générer un composant couleur
+    const randomValue = () => Math.floor(Math.random() * 256).toString(16).padStart(2, '0');
+    // Retourner la couleur complète
+    return `#${randomValue()}${randomValue()}${randomValue()}`;
+}
+
+// Écouteur d'événement pour le bouton de couleurs aléatoires
+randomButton.addEventListener('click', function(event) {
+    // Générer et stocker les couleurs aléatoires
+    firstColorValue = randomHexColor();
+    secondColorValue = randomHexColor();
+    // Mettre à jour les inputs avec les couleurs générées
+    firstColorInput.value = firstColorValue;
+    secondColorInput.value = secondColorValue;
+    // Afficher les couleurs générées
+    firstColorValueDisplay.textContent = firstColorValue.toUpperCase();
+    secondColorValueDisplay.textContent = secondColorValue.toUpperCase();
+    // Appliquer le nouveau gradient
+    updateGradient();
+});
---
+++ consignes/consignes.md
@@ -0,0 +1,23 @@
+# Générateur de dégradés 🖌️
+
+ctrl+shift+v sur VS Code pour analyser ce fichier markdown
+
+  Votre mission est de coder un générateur de dégradés.
+  Vous allez manipuler des inputs de couleurs afin de créer des "linear-gradient" à la volée !
+
+### A. Coder une interface basique
+Codez d'abord une interface très simple, contenant les éléments importants : boutons, inputs, liens, etc...
+Rajoutez un peu de style si besoin est. 
+
+Puis codez les fonctionnalités JavaScript.
+
+### B. Fonctionnalités JavaScript à coder pour ce projet
+
+1. Gérez l'implémentation de base des couleurs, il faut qu'il y est un dégradé lorsqu'on arrive sur le site (input, orientation, body...).
+2. Gérez le changement de couleur, on doit pouvoir manipuler les inputs et provoquer le changement de couleur du site.
+3. Occupez-vous de l'inclinaison avec l'input type "range".
+4. Mettez en place la copie du dégradé en cliquant su le bouton "Copier le gradient".
+5. Faites-en sorte de créer des dégradés au hasard en cliquant sur le bouton "random".
+6. Bonne chance ! 
+
+### C. Ajoutez du style à l'interface afin de terminer le projet.
